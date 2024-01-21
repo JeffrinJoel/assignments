93a1d40a371a6243e9835c236f8af18dbb9a0727
Jeffrin Joel Raj <111161944+JeffrinJoel@users.noreply.github.com>
2024-01-21 22:14:57 +0530

    Add files via upload

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod = require('zod')
 
 /**
  * Generates a JWT for a given username and password.
@@ -13,8 +13,18 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username)
+    const passwordResponse = passwordSchema.safeParse(password)
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+    const signature = jwt.sign({username}, jwtPassword)
+    return signature
 }
 
 /**
@@ -26,8 +36,16 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    try{
+        const verified = jwt.verify(token, jwtPassword);
+        if(verified){
+            return true;
+        }
+    }catch(e){
+        return false;
+    }
 }
+   
 
 /**
  * Decodes a JWT to reveal its payload without verifying its authenticity.
@@ -37,7 +55,12 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    const decoded = jwt.decode(token)
+    if(decoded){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
